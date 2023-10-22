9c8ed0963e6e63fd425233c65cc9c5f481b762a1
koturn <jeak.koutan.apple@gmail.com>
2023-10-22 19:41:16 +0900

    feat: Implement export tool

---
+++ Assets/koturn/Tools/KRayMarching/Editor/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyCompany("koturn")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCopyright("Copyright (C) 2023 koturn All Rights Reserverd.")]
+[assembly: AssemblyDescription("Utilities for koturn/KRayMarching.")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: AssemblyProduct("Koturn.Tools.KRayMarching")]
+[assembly: AssemblyTitle("Koturn.Tools.KRayMarching")]
+[assembly: AssemblyMetadata("RepositoryUrl", "https://github.com/koturn/KRayMarching")]
