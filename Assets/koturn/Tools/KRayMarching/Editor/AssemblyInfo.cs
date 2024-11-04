ac7820f515c5a5c578f059b38b08f5375a40ec85
koturn <jeak.koutan.apple@gmail.com>
2024-11-05 05:36:15 +0900

    chore: Refine AssemblyInfo.cs

--- Assets/koturn/Tools/KRayMarching/Editor/AssemblyInfo.cs
+++ Assets/koturn/Tools/KRayMarching/Editor/AssemblyInfo.cs
@@ -1,12 +1,36 @@
 using System.Reflection;
+using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyCompany("koturn")]
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Koturn.Tools.KRayMarching")]
+[assembly: AssemblyDescription("Export tool for koturn/KRayMarching.")]
 [assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("koturn")]
+[assembly: AssemblyProduct("Koturn.Tools.KRayMarching")]
 [assembly: AssemblyCopyright("Copyright (C) 2023 koturn All Rights Reserverd.")]
-[assembly: AssemblyDescription("Utilities for koturn/KRayMarching.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b27c699a-7511-cf94-589f-a62add812b98")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
-[assembly: AssemblyProduct("Koturn.Tools.KRayMarching")]
-[assembly: AssemblyTitle("Koturn.Tools.KRayMarching")]
+
+// Metadata
 [assembly: AssemblyMetadata("RepositoryUrl", "https://github.com/koturn/KRayMarching")]
